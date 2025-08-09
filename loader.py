fb7e71bf86b10f72d8dafcb6d78cbe6ad75cf506
tpk256 <tpk256@mail.ru>
2025-08-09 15:58:58 +0500

    test-v

--- loader.py
+++ loader.py
@@ -5,8 +5,8 @@ from aiogram import Bot
 class SingleBot:
     bot: Bot = None
 
-async def load_file(path, filename, chat_id: int) -> str:
-    conf = FSInputFile(path)
+async def load_file(path, client_name, chat_id: int) -> str:
+    conf = FSInputFile(path, client_name)
     data = await SingleBot.bot.send_document(
         chat_id=chat_id,
         document=conf
